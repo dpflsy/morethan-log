70a2c174ecfa0f4ffd8c418c72b80bdc35ca6fed
dpflsy25 <dpflsy25@gmail.com>
2023-06-21 11:47:00 +0900

    update profile image

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Yerin",
-    image: "/yerin.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/notion-avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "backend developer",
     bio: "Be a better developer!!",
     email: "dpflsy25@gmail.com",
